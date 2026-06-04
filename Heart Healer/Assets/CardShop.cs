c3c649046101063bd9116f80a8e677d582f787c2
udan321 <kimminsik6126@gmail.com>
2026-06-04 20:08:54 +0900

    --

--- Heart Healer/Assets/CardShop.cs
+++ Heart Healer/Assets/CardShop.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 public class CardShop : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    //상점을 불러올 이미지를 버튼역할을 하게하고 전체 카드가 보관되어 있는 에셋에서 랜덤으로 8장 불러오고 그중에 구매할수 있게 한다 재화가 없으면 구매 불가(상점이불러져 올때 가격은 카드 등급이 있나 있으면 등급에 맞춰 가격을 정함 아니면 모두 랜덤)
-    //상점은 상점 바탕이미지 오른쪽 상단에 x이미지를 버튼으로 만들어서 이미지를 닫게 함 닫으면 상점 초기화
+   
     void Start()
     {
         
